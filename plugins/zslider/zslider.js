f72801fba4d4cd9080eca9163b7709d5a3d225c5
Archer_小A <qddegtya@gmail.com>
2016-01-04 13:40:49 +0800

    Update zslider.js

--- plugins/zslider/zslider.js
+++ plugins/zslider/zslider.js
@@ -8,55 +8,49 @@
 
     'use strict';
 
-    //检查插件环境
+    // 检查插件环境
     if(typeof Zepto === "undefined") { throw new Error("请引入zepto"); }
 
-    //可用变量
     var options = {};
     var self;
-    var screenWidth; //屏幕宽度px值，用于样式调整
-    var zContainer; //主容器
-    var currentBannerIndex = 0; //容器一开始所在位置
-    var screenWidthNum;  //屏幕宽度int值,用于计算
-    var originX; //记录手指初始坐标
-    var isToLeft; //记录向左滑动还是向右滑动
-    var currentBanner; //手指当前banner
-    //var bannerLi = $(".z_slider_li");
-    var globalTimer; //全局定时器
-    var currentTimer = 0; //当前定时器指针
+    var screenWidth; // 屏幕宽度px值，用于样式调整
+    var zContainer; // 主容器
+    var currentBannerIndex = 0; // 容器一开始所在位置
+    var screenWidthNum;  // 屏幕宽度int值,用于计算
+    var originX; // 记录手指初始坐标
+    var isToLeft; // 记录向左滑动还是向右滑动
+    var currentBanner; // 手指当前banner
+    var globalTimer; // 全局定时器
+    var currentTimer = 0; // 当前定时器指针
 
     $.fn.zSlider = function(userOptions){
-        //确保options不为空
         options = $.extend({}, $.fn.zSlider._initialOptions, userOptions);
-
-        //绑定调用者
+        // 绑定调用者
         self = this;
-
-        //执行
         $.fn.zSlider.init();
     };
 
-    //初始化配置
+    // 初始化配置
     $.fn.zSlider._initialOptions = {
-        "bannerPreImg" : "", //Banner默认占位图
-        "hasControl": true, //默认生成控制器
-        "speed" : 2, //播放速度
-        "bannerList" :[] //banner列表
+        "bannerPreImg" : "", // Banner默认占位图
+        "hasControl": true, // 默认生成控制器
+        "speed" : 2, // 播放速度
+        "bannerList" :[] // banner列表
     };
 
     $.fn.zSlider.init = function(){
 
-        //获取屏幕宽度
+        // 获取屏幕宽度
         screenWidthNum = $(window).width();
         screenWidth = $(window).width() + "px";
 
-        //获取百分比宽度基数
+        // 获取百分比宽度基数
         var _perW = options.bannerList.length;
 
-        //设置宽度
+        // 设置宽度
         self.css({width: _perW * 100 + "%"});
 
-        //装载,并初始化宽度
+        // 装载,并初始化宽度
         $.each(options.bannerList, function(index, imgUrl){
 
             var _li = document.createElement("li");
@@ -73,7 +67,7 @@
 
         });
 
-        //显示控制条
+        // 显示控制条
         if(options.hasControl || false){
 
             var _controlP = document.createElement("ul");
@@ -88,7 +82,7 @@
                 _li.dataset.navIndex = index; //将navIndex塞入,用于后续处理
                 _li.style.width = (1 / options.bannerList.length) * 100 + "%";
 
-                //如果等于当前序号，则添加样式
+                // 如果等于当前序号，则添加样式
                 if(index === currentBannerIndex){
                     $(_li).addClass("currentNav");
                 }
@@ -97,18 +91,18 @@
 
             });
 
-            //控制区域需要添加到主容器
+            // 控制区域需要添加到主容器
             zContainer = $("#z_slider_container");
             zContainer.append(_controlP);
 
-            //控制条处理
+            // 控制条处理
             $(".z_slider_li").on("click", function(e){
 
-                //计算需要的位移
+                // 计算需要的位移
                 var _m = parseInt($(this).attr("data-nav-index"));
                 var _transOffset = 0 - (_m * screenWidthNum);
 
-                //加入动画
+                // 加入动画
                 self.css({
                     "-webkit-transition": "transform 0.4s",
                     "-moz-transition": "transform 0.4s",
@@ -117,10 +111,10 @@
                     "transition": "transform 0.4s"
                 });
 
-                //处理3d滑动
+                // 处理3d滑动
                 zTranslate3d(self, _transOffset);
 
-                //处理样式
+                // 处理样式
                 $(this).addClass("currentNav");
                 $(this).siblings().removeClass("currentNav");
 
@@ -128,11 +122,11 @@
 
         }
 
-        //定时自动滑动
+        // 定时自动滑动
         if(options.autoPlay && true){
             (function(c, l){
 
-                //加入动画
+                // 加入动画
                 self.css({
                     "-webkit-transition": "transform 0.4s",
                     "-moz-transition": "transform 0.4s",
@@ -144,7 +138,7 @@
                 globalTimer = setInterval(function(){
                     zTranslate3d(self, 0 - c * screenWidthNum);
 
-                    //处理nav
+                    // 处理nav
                     $($(".z_slider_li")[c]).addClass("currentNav");
                     $($(".z_slider_li")[c]).siblings().removeClass("currentNav");
 
@@ -160,51 +154,42 @@
             }(currentTimer, options.bannerList.length));
         }
 
-        //绑定触摸事件流
-        self.on("touchstart", function(event){ zTouchHandlerStart(event); });
-        self.on("touchmove", function(event){ zTouchHandlerMove(event); });
-        self.on("touchend", function(event){ zTouchHandlerEnd(event); });
+        // 绑定触摸事件流
+        self.on("touchstart", zTouchHandlerStart);
+        self.on("touchmove", zTouchHandlerMove);
+        self.on("touchend", zTouchHandlerEnd);
 
     };
 
 
-    //手指开始放到屏幕上
+    // 手指开始放到屏幕上
     function zTouchHandlerStart(e){
-
-        //清除定时器
-        //clearInterval(globalTimer);
-
         e.preventDefault();
 
-        //保证只有一个触点
+        // 保证只有一个触点
         if(e.targetTouches.length > 1 || e.touches.length > 1){
             return;
         }
 
-        //判断tagName
-        //if(e.target.tagName.toLowerCase() === "img"){
+        currentBanner = e.target;
+        currentBannerIndex = parseInt($(currentBanner).attr("data-banner-index"));
 
-            currentBanner = e.target;
-            currentBannerIndex = parseInt($(currentBanner).attr("data-banner-index"));
-
-            //保存初始值
-            originX = e.targetTouches[e.targetTouches.length - 1].screenX;
-
-        //}
+        // 保存初始值
+        originX = e.targetTouches[e.targetTouches.length - 1].screenX;
 
     }
 
-    //手指开始移动
+    // 手指开始移动
     function zTouchHandlerMove(e){
 
         e.preventDefault();
 
-        //清除转场动画
+        // 清除转场动画
         self.css({
             "-webkit-transition": ""
         });
 
-        //左滑还是右滑用于离开时做响应
+        // 左滑还是右滑用于离开时做响应
         if(e.changedTouches[e.changedTouches.length - 1].screenX > originX){
             isToLeft = false;
         } else if(e.changedTouches[e.changedTouches.length - 1].screenX < originX){
@@ -212,7 +197,7 @@
         }
 
 
-        //是否开启拖拽
+        // 是否开启拖拽
         if(options.dragEnable){
 
             if(isToLeft){
@@ -236,7 +221,7 @@
 
         e.preventDefault();
 
-        //加入动画
+        // 加入动画
         self.css({
             "-webkit-transition": "transform 0.4s",
             "-moz-transition": "transform 0.4s",
@@ -246,7 +231,7 @@
         });
 
         if(e.changedTouches[0].screenX === originX){
-            //这是一次tap事件
+            // 这是一次tap事件
             return;
         }
 
@@ -255,7 +240,7 @@
                 zTranslate3d(self, 0 - (currentBannerIndex + 1) * screenWidthNum);
             }
 
-            //处理nav
+            // 处理nav
             $($(".z_slider_li")[currentBannerIndex + 1]).addClass("currentNav");
             $($(".z_slider_li")[currentBannerIndex + 1]).siblings().removeClass("currentNav");
 
@@ -264,21 +249,17 @@
                 zTranslate3d(self, 0 - (currentBannerIndex - 1) * screenWidthNum);
             }
 
-            //处理nav
+            // 处理nav
             $($(".z_slider_li")[currentBannerIndex - 1]).addClass("currentNav");
             $($(".z_slider_li")[currentBannerIndex - 1]).siblings().removeClass("currentNav");
         }
     }
 
 
-    //translate3D,用于banner整体移位动画
+    // translate3D,用于banner整体移位动画
     function zTranslate3d(ele, offset){
         $(ele).css({
-            //"transform": "translate3d(" + offset + "px, 0, 0)",
             "-webkit-transform": "translate3d(" + offset + "px, 0, 0)"
-            //"-moz-transform": "translate3d(" + offset + "px, 0, 0)",
-            //"-o-transform": "translate3d(" + offset + "px, 0, 0)",
-            //"-ms-transform": "translate3d(" + offset + "px, 0, 0)"
         });
     }
 
